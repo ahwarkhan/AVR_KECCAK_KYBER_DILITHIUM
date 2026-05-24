f18d4aaae998c89386333b35953a9c4382072fe3
ahwarkhan <khanahwar4@gmail.com>
2026-05-24 18:05:22 +0530

    C Ref Implementation of Kyber768 for AVR

--- kyber/ref/kyber768/api.h
+++ kyber/ref/kyber768/api.h
@@ -1,20 +1,18 @@
-#ifndef API_H
-#define API_H
+#ifndef PQCLEAN_KYBER768_CLEAN_API_H
+#define PQCLEAN_KYBER768_CLEAN_API_H
 
 #include <stdint.h>
-#include "params.h"
 
-#define CRYPTO_SECRETKEYBYTES KYBER_SECRETKEYBYTES
-#define CRYPTO_PUBLICKEYBYTES KYBER_PUBLICKEYBYTES
-#define CRYPTO_CIPHERTEXTBYTES KYBER_CIPHERTEXTBYTES
-#define CRYPTO_BYTES KYBER_SSBYTES
-
-#define crypto_kem_keypair PQCLEAN_KYBER768_CLEAN_crypto_kem_keypair
-#define crypto_kem_enc PQCLEAN_KYBER768_CLEAN_crypto_kem_enc
-#define crypto_kem_dec PQCLEAN_KYBER768_CLEAN_crypto_kem_dec
+#define PQCLEAN_KYBER768_CLEAN_CRYPTO_SECRETKEYBYTES  2400
+#define PQCLEAN_KYBER768_CLEAN_CRYPTO_PUBLICKEYBYTES  1184
+#define PQCLEAN_KYBER768_CLEAN_CRYPTO_CIPHERTEXTBYTES 1088
+#define PQCLEAN_KYBER768_CLEAN_CRYPTO_BYTES           32
+#define PQCLEAN_KYBER768_CLEAN_CRYPTO_ALGNAME "Kyber768"
 
 int PQCLEAN_KYBER768_CLEAN_crypto_kem_keypair(uint8_t *pk, uint8_t *sk);
+
 int PQCLEAN_KYBER768_CLEAN_crypto_kem_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
+
 int PQCLEAN_KYBER768_CLEAN_crypto_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
 
 #endif
